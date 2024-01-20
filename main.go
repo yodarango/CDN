5157a2ac84c3358ccdbfefc825489d2522e304a1
Daniel Rangel <danranngel@gmail.com>
2024-01-20 16:21:47 -0500

    fixed font coloring

--- main.go
+++ main.go
@@ -11,4 +11,5 @@ utils.BundleAndMinifySingleCSSFile("./src/CSS/ds.css", disPath)
 utils.BundleAndMinifySingleCSSFile("./src/CSS/icons.css", disPath)
 utils.BundleAndMinifySingleCSSFile("./src/CSS/tokens.css", disPath)
 utils.BundleAndMinifySingleCSSFile("./src/CSS/utils.css", disPath)
+utils.LoopOverIcons("./src/CSS/icons.css", "./src/JS/icons.js")
 }
