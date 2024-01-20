677a13fa72f48888fcd3b3d89ff6eb85a7d502ce
Daniel Rangel <danranngel@gmail.com>
2024-01-20 12:46:03 -0500

    added tokens.js

---
+++ src/CSS/tokens.js
@@ -0,0 +1,38 @@
+export const colorScheme = "dark";
+
+export const DR_ALPHA = "#f1eaff"; /* app lettering */
+export const DR_ALPHA_RGB = "241, 234, 255";
+export const DR_BETA = "#130e21"; /* app background */
+export const DR_BETA_RGB = "19, 14, 33";
+export const DR_GAMMA = "#201a30"; /* app foreground contrast */
+export const DR_GAMMA_RGB = "32, 26, 48";
+export const DR_DELTA = "#5200ff";
+export const DR_DELTA_RGB = "82, 0, 255";
+export const DR_EPSILON = "#763ffb";
+export const DR_EPSILON_RGB = "118, 63, 251";
+export const DR_ZETA = "#b293fe";
+export const DR_ZETA_RGB = "178, 147, 254";
+export const DR_ETA = "#ff4d62";
+export const DR_ETA_RGB = "255, 77, 98";
+export const DR_THETA = "#ffc107";
+export const DR_THETA_RGB = "255, 193, 7";
+export const DR_IOTA = "#8d82a8";
+export const DR_IOTA_RGB = "141, 130, 168";
+export const DR_KAPPA = "#6af548";
+export const DR_KAPPA_RGB = "106, 245, 72";
+export const DR_LAMBDA = "#07a0ff";
+export const DR_LAMBDA_RGB = "7, 160, 255";
+export const DR_MU = "#350c8cab";
+export const DR_MU_RGB = "53, 12, 140, 0.67";
+export const DR_NU = "transparent";
+export const DR_NU_RGB = "0, 0, 0, 0";
+
+/* stateful colors */
+export const DR_SUCCESS = DR_KAPPA;
+export const DR_SUCCESS_RGB = DR_KAPPA_RGB;
+export const DR_INFO = DR_LAMBDA;
+export const DR_INFO_RGB = DR_LAMBDA_RGB;
+export const DR_WARNING = DR_THETA;
+export const DR_WARNING_RGB = DR_THETA_RGB;
+export const DR_DANGER = "#ff4d62";
+export const DR_DANGER_RGB = DR_ETA_RGB;
