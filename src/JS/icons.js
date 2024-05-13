71831a3b443564ec77605491b4231ac9e9e83f12
daniel rangel <yodarango@Rooky.lan>
2024-05-13 19:29:13 -0400

    modified icon.js

--- src/JS/icons.js
+++ src/JS/icons.js
@@ -421,16 +421,26 @@ const svgMap = {
   "wine-outline": `<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><path d="M398.57,80H113.43V96S87.51,272,256,272,398.57,96,398.57,96Z" style="fill:none;stroke:#000;stroke-linejoin:round;stroke-width:32px"/><line x1="256" y1="272" x2="256" y2="432" style="fill:none;stroke:#000;stroke-linejoin:round;stroke-width:32px"/><line x1="352" y1="432" x2="160" y2="432" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/><line x1="112" y1="160" x2="400" y2="160" style="fill:none;stroke:#000;stroke-linejoin:round;stroke-width:32px"/></svg>`,
   "woman-outline": `<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><path d="M208,368V472a23.73,23.73,0,0,0,24,24h0a23.73,23.73,0,0,0,24-24" style="fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px"/><path d="M256,368V472a23.73,23.73,0,0,0,24,24h0a23.73,23.73,0,0,0,24-24V368" style="fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px"/><path d="M183,274a23.73,23.73,0,0,1-29.84,16.18h0a23.72,23.72,0,0,1-16.17-29.84l25-84.28A44.85,44.85,0,0,1,205,144H307a44.85,44.85,0,0,1,43,32.08l25,84.28a23.72,23.72,0,0,1-16.17,29.84h0A23.73,23.73,0,0,1,329.05,274" style="fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px"/><circle cx="256" cy="56" r="40" style="fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px"/><polyline points="208 192 160 352 352 352 304 192" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>`,
 };
+
 let count = 0;
+let interval;
+
 function replaceIcons() {
-  const e = document.querySelectorAll(".icon");
-  if ((console.log("replaceIcons", count), !e)) {
-    replaceIcons(), count++;
-    return;
-  }
-  e.forEach((e) => {
-    const t = e.getAttribute("class");
-    t && (e.innerHTML = svgMap[t]);
-  });
+  const icons = document.querySelectorAll(".icon");
+
+  interval = setInterval(() => {
+    if (count >= icons.length) {
+      icons.forEach((icon) => {
+        const iconClass = icon
+          .getAttribute("class")
+          .find((c) => c.startsWith(".icon-"));
+        icon.innerHTML = icons[iconClass];
+      });
+
+      count++;
+      clearInterval(interval);
+    }
+  }, 100);
 }
+
 window.addEventListener("DOMContentLoaded", replaceIcons);
